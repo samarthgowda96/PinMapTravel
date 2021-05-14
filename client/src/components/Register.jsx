23d3d89e082cae41c28e14414cbe3fbffa773267
Samarth Gowda <samarthgowda@Samarths-MBP.fios-router.home>
2021-05-14 18:15:50 -0400

    working registration module

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,15 +1,52 @@
-import './register.css'
+import './register.css';
+import { Room, Cancel} from "@material-ui/icons";
+import {useRef,useState} from 'react';
+import axios from 'axios';
 
 export default function Register(){
+    const [success,setSuccess]=useState(false);
+    const [error,setError]=useState(false);
+    const nameRef= useRef();
+    const emailRef= useRef();
+    const passwordRef= useRef();
+
+    const handleSubmit= async (e)=>{
+        e.preventDefault();
+        const newUser={
+            username:nameRef.current.value,
+            email:emailRef.current.value,
+            password:passwordRef.current.value,
+        }
+        try {
+            const res = await axios.post("http://localhost:6900/api/users/register",newUser)
+            setError(false)
+            setSuccess(true)
+            
+        } catch (error) {
+            
+           setError(true)
+        }
+
+    }
+
+
     return(
         <div className="registerContainer">
-            <div className="logo"></div>
-            <form>
-                <input type="text" placeholder="username"></input>
-                <input type="email" placeholder="email"></input>
-                <input type="password" placeholder="password"></input>
-                <button>Register</button>
+            <div className="logo">
+                <Room/>
+                Sammy's Pin Map
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="username" ref={nameRef}></input>
+                <input type="email" placeholder="email" ref={emailRef}></input>
+                <input type="password" placeholder="password" ref={passwordRef}></input>
+                <button className="registerBtn">Register</button>
+                {success &&
+                <span className="success"> Successfull. You can login Now!</span>
+                }{error &&
+                <span className="failure"> Failed. Retry</span>}
             </form>
+            <Cancel className="registerCancel"/>
 
         </div>
     )
